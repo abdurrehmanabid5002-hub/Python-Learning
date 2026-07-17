6d1a4275923ba550855be1fd5882def5f8ee9d71
AbdurRehmanAbid <abdurrehmanabid5002@gmail.com>
2026-07-17 18:45:48 +0500

    initial commit

---
+++ learn.py
@@ -0,0 +1,16 @@
+# # print("Helo World "
+# name = input("enter your name : ")
+# print("your name is :",name )
+# letter = "I am a good boy"
+# print (letter.replace ("good", "bad"))
+# print (letter)
+# a= int (input("enter a number : " ))
+
+
+# b= int (input("enter a number : " ))
+# print ( a*b)
+
+
+letter = "i am a good boy and ali is not a good boy "
+print (letter.capitalize().replace("good", "bad").replace("ali", "ahmad"))
+print(letter)
