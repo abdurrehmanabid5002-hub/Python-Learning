a2c6fd227fc43cf9af14003aeb3cd5b48108c242
AbdurRehmanAbid <abdurrehmanabid5002@gmail.com>
2026-07-19 20:29:03 +0500

    code

--- problems.py
+++ problems.py
@@ -1,8 +1,9 @@
-while True:
+i = 1
+while i <= 2:
     marks = []
 
-    for i in range(3):
-        marks.append(int(input(f"Enter marks {i + 1}: ")))
+    for j in range(3):
+        marks.append(int(input(f"Enter marks {j + 1}: ")))
 
     marks1 = marks[0]
     marks2 = marks[1]
@@ -14,3 +15,4 @@ while True:
         print("You are pass", total_percentage)
     else:
         print("You are fail", total_percentage)
+    i += 1
