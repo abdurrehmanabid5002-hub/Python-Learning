c50df14fd3ec7727d33d2b2d9de43ac288333940
AbdurRehmanAbid <abdurrehmanabid5002@gmail.com>
2026-07-20 17:32:25 +0500

    code

---
+++ patterns.py
@@ -0,0 +1,25 @@
+i = 1
+while True:
+    n = int(input("enter a number : "))
+    if n == 0:
+        break
+    else:
+        for i in range(1, n + 1):
+            print(" " * (n - i), end="")
+            print("*" * (2 * i - 1))
+            # print("")
+
+
+i = 1
+while True:
+    n = int(input("enter a number : "))
+    if n == 0:
+        break
+    for i in range(1, n+1):
+        if i == 1 or i == n:
+            print("*" * n, end="")
+        else:
+            print("*", end="")
+            print(" " * (n - 2), end="")
+            print("*", end="")
+        print("")
