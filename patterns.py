c77a954312251dbd16f6463037ab8f6e1029b9d4
AbdurRehmanAbid <abdurrehmanabid5002@gmail.com>
2026-07-20 17:39:24 +0500

    code

--- patterns.py
+++ patterns.py
@@ -1,13 +1,28 @@
-i = 1
-while True:
-    n = int(input("enter a number : "))
-    if n == 0:
-        break
-    else:
-        for i in range(1, n + 1):
-            print(" " * (n - i), end="")
-            print("*" * (2 * i - 1))
-            # print("")
+# i = 1
+# while True:
+#     n = int(input("enter a number : "))
+#     if n == 0:
+#         break
+#     else:
+#         for i in range(1, n + 1):
+#             print(" " * (n - i), end="")
+#             print("*" * (2 * i - 1))
+#             # print("")
+
+
+# i = 1
+# while True:
+#     n = int(input("enter a number : "))
+#     if n == 0:
+#         break
+#     for i in range(1, n+1):
+#         if i == 1 or i == n:
+#             print("*" * n, end="")
+#         else:
+#             print("*", end="")
+#             print(" " * (n - 2), end="")
+#             print("*", end="")
+#         print("")
 
 
 i = 1
@@ -15,11 +30,5 @@ while True:
     n = int(input("enter a number : "))
     if n == 0:
         break
-    for i in range(1, n+1):
-        if i == 1 or i == n:
-            print("*" * n, end="")
-        else:
-            print("*", end="")
-            print(" " * (n - 2), end="")
-            print("*", end="")
-        print("")
+    for i in range (1,11 ):
+        print (f"{n}x{i}={n*i}")
