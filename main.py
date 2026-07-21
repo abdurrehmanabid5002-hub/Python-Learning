fd60e60da3cce4b25dce7fc80da2b556c7dda527
AbdurRehmanAbid <abdurrehmanabid5002@gmail.com>
2026-07-21 13:50:10 +0500

    comments

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 import random
 
 youstr = input("Enter a number :")
+
 computer = random.choice([-1, 0, 1])
 youdict = {"s": 1, "w": -1, "g": 0}
 reversedict = {1: "snake", -1: "water", 0: "gun"}
@@ -15,17 +16,17 @@ print(f"you chose {reversedict[you]  }\nComputer chose {reversedict[you ]}")
 if computer == you:
     print("you draw!")
 else:
-    if computer == -1 and you == 1:
+    if computer == -1 and you == 1:  # -2
         print("you win!")
-    elif computer == -1 and you == 0:
+    elif computer == -1 and you == 0:  # -1
         print("you win!")
-    elif computer == 1 and you == -1:
+    elif computer == 1 and you == -1:  # 2
         print("you lose!")
-    elif computer == 1 and you == 0:
+    elif computer == 1 and you == 0:  # 1
         print("you win!")
-    elif computer == 0 and you == 1:
+    elif computer == 0 and you == 1:  # -1
         print("you lose!")
-    elif computer == 0 and you == -1:
+    elif computer == 0 and you == -1:  # 1
         print("you lose!")
     else:
         print("something went wrong !")
