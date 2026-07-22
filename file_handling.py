a1c7c996818fb94f453a84735de823dfccab6a4a
AbdurRehmanAbid <abdurrehmanabid5002@gmail.com>
2026-07-22 18:40:52 +0500

    code

--- file_handling.py
+++ file_handling.py
@@ -1,8 +1,23 @@
-# f =open("file.txt","r") 
-# data = f.read()
-# print(data)
-# f.close()
+f =open("file.txt","r") 
+data = f.read()
+print(data)
+f.close()
 
 with open ("file.txt ","r") as f :
     data = f.read()
     print( data)
+
+str = " i am fine how are you hello  "
+f=open( "myfile.txt","w")
+f.write (str)
+f.close 
+
+
+# f =open("file.txt") 
+# line = f.readlines()
+# # print ( line , type(line ))
+# while ( line !=""):
+#     print ( line )
+#     # line= f.readlines()
+# f. close()
+
