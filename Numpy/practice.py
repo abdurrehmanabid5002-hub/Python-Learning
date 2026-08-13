042ac804374c628ecadc4fb93a0309f423de9f75
AbdurRehmanAbid <abdurrehmanabid5002@gmail.com>
2026-08-13 21:56:00 +0500

    psl data analysis

--- Numpy/practice.py
+++ Numpy/practice.py
@@ -1,7 +1,12 @@
 import numpy as np 
+import pandas as pd 
 
 
 
 array4 = np.arange(1000000000)
 
 print(array4)
+
+
+df= pd.read_csv(r"C:\Users\abdur\Downloads\archive (1)\psl2026_matches.csv")
+df.head()
