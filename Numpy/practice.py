89f0a9a90607bfe6f645c2279d1fe85e3d7c20e3
AbdurRehmanAbid <abdurrehmanabid5002@gmail.com>
2026-08-20 23:22:52 +0500

    animation

--- Numpy/practice.py
+++ Numpy/practice.py
@@ -1,4 +1,4 @@
-import numpy as np 
+'''import numpy as np 
 import pandas as pd 
 import tkinter as tk
 
@@ -56,4 +56,46 @@ def animate():
 animate()
 
 # Start Tkinter
-window.mainloop()
+window.mainloop()'''
+
+
+
+
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+
+app = Dash(__name__)
+
+
+app.layout = html.Div([
+    html.H4('Animated GDP and population over decades'),
+    html.P("Select an animation:"),
+    dcc.RadioItems(
+        id='selection',
+        options=["GDP - Scatter", "Population - Bar"],
+        value='GDP - Scatter',
+    ),
+    dcc.Loading(dcc.Graph(id="graph"), type="cube")
+])
+
+
+@app.callback(
+    Output("graph", "figure"),
+    Input("selection", "value"))
+def display_animated_graph(selection):
+    df = px.data.gapminder() # replace with your own data source
+    animations = {
+        'GDP - Scatter': px.scatter(
+            df, x="gdpPercap", y="lifeExp", animation_frame="year",
+            animation_group="country", size="pop", color="continent",
+            hover_name="country", log_x=True, size_max=55,
+            range_x=[100,100000], range_y=[25,90]),
+        'Population - Bar': px.bar(
+            df, x="continent", y="pop", color="continent",
+            animation_frame="year", animation_group="country",
+            range_y=[0,4000000000]),
+    }
+    return animations[selection]
+
+
+app.run(debug=True)
