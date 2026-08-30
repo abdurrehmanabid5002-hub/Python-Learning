ed82f7a4d63a58b22b7490effdcd3faaad2a70db
AbdurRehmanAbid <abdurrehmanabid5002@gmail.com>
2026-08-30 21:01:40 +0500

    code

--- Python Learning/problems.py
+++ Python Learning/problems.py
@@ -23,3 +23,16 @@ for i in range (100):
     if i==34:
         continue
     print (i)
+
+
+
+
+# ==================Fibonacci Generator====================
+n = int(input("How many numbers do you want? "))
+
+a = 0
+b = 1
+
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
