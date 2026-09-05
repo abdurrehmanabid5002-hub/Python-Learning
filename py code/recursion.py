8148734a0fd75132074a8f83a7e74359446befe5
AbdurRehmanAbid <abdurrehmanabid5002@gmail.com>
2026-09-05 21:38:15 +0500

    code

--- py code/recursion.py
+++ py code/recursion.py
@@ -8,3 +8,35 @@ def countdown(n):
 
 
 countdown(5)
+
+
+
+def binary_search(numbers, target):
+    left = 0
+    right = len(numbers) - 1
+
+    while left <= right:
+        middle = (left + right) // 2
+
+        if numbers[middle] == target:
+            return middle
+
+        elif numbers[middle] < target:
+            left = middle + 1
+
+        else:
+            right = middle - 1
+
+    return -1
+
+
+numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45]
+
+target = 40
+
+result = binary_search(numbers, target)
+
+if result != -1:
+    print("Found at index:", result)
+else:
+    print("Number not found.")
