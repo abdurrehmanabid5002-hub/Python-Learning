0b74962630cdf7c0f7c75b7617cd0df770a7fe2b
AbdurRehmanAbid <abdurrehmanabid5002@gmail.com>
2026-09-21 21:43:42 +0500

    code

---
+++ Games/rock.py
@@ -0,0 +1,24 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+computer = random.choice(choices)
+
+player = input("Choose rock, paper or scissors: ").lower()
+
+print("Computer:", computer)
+
+if player == computer:
+    print("Draw!")
+
+elif player == "rock" and computer == "scissors":
+    print("You win!")
+
+elif player == "paper" and computer == "rock":
+    print("You win!")
+
+elif player == "scissors" and computer == "paper":
+    print("You win!")
+
+else:
+    print("Computer wins!")
